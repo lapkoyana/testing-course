bdca5ca87ad65cf57c3a09fc182607f85bb66fb0
lapkoyana <lapko.yan@gmail.com>
2021-07-14 12:32:54 +0400

    Add task4-1

--- sandbox/src/main/java/task1/pack/MainClass.java
+++ sandbox/src/main/java/task1/pack/MainClass.java
@@ -1,19 +1,17 @@
 package task1.pack;
 
 public class MainClass {
-	
 	public static void main(String[] args) {
-		System.out.println("Some text");
+	System.out.println("Some text");
 		
 		Point p1 = new Point(1,2);
-		System.out.println("РџРµСЂРІР°СЏ С‚РѕС‡РєР° СЃ РєРѕРѕСЂРґРёРЅР°С‚Р°РјРё (" + p1.x + ", " + p1.y + ")");
+		System.out.println("Первая точка с координатами - (" + p1.x + ", " + p1.y + ")");
 		
 		Point p2 = new Point(1, 3);
-		System.out.println("Р’С‚РѕСЂР°СЏ С‚РѕС‡РєР° СЃ РєРѕРѕСЂРґРёРЅР°С‚Р°РјРё (" + p2.x + ", " + p2.y + ")");
+		System.out.println("Вторая точка с координатами - (" + p2.x + ", " + p2.y + ")");
 		
-		System.out.println("Р Р°СЃСЃС‚РѕСЏРЅРёРµ РјРµР¶РґСѓ РґРІСѓРјСЏ С‚РѕС‡РєР°РјРё - " + p1.distance(p2));
+		System.out.println("Расстояние между точками - " + p1.distance(p2));
 	}
-	
 	public static double distance(Point p1, Point p2) {
 		return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
 	}
